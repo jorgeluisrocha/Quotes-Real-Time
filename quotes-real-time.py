59705034d34ff3f33a5def67540aa9d40a154303
jorgeluisrocha <jlr435@cornell.edu>
2016-07-18 20:17:49 -0400

    fixes the design of the program.

--- quotes-real-time.py
+++ quotes-real-time.py
@@ -1,14 +1,18 @@
 from googlefinance import getQuotes
 from datetime import datetime as dt
 
-quotes = ['AAPL', 'GOOG', 'MSFT', 'TSLA'] ## Here are the quotes desired.
-prices = [0,0,0,0] ## Intializes the prices list.
-prevtime = [dt.utcnow().minute, dt.utcnow().second, dt.utcnow().microsecond]  ## Our comparitor
-for i in range(20):
-    time = [dt.utcnow().minute, dt.utcnow().second, dt.utcnow().microsecond]  ## 'Present' time
-    if time[2] > prevtime[2] or time[1] > prevtime[1]: ## Compares our time
-    	q = getQuotes(quotes)
-    	for j in range(len(q)):
-    		prices[j] = q[j]['LastTradePrice']
-    	print(prices)
-    	prevtime = time
+def get_quotes(quotes):
+	prices = [0,0,0,0,0] ## Intializes the prices list.
+	prevtime = [dt.utcnow().minute, dt.utcnow().second, dt.utcnow().microsecond]  ## Our comparitor
+	for i in range(20):
+	    time = [dt.utcnow().minute, dt.utcnow().second, dt.utcnow().microsecond]  ## 'Present' time
+	    if time[2] > prevtime[2] or time[1] > prevtime[1]: ## Compares our time
+	    	q = getQuotes(quotes)
+	    	for j in range(len(q)):
+	    		prices[j] = q[j]['LastTradePrice']
+	    	print(prices)
+	    	prevtime = time
+
+if __name__ == "__main__":
+	quotes = ['AAPL', 'GOOG', 'MSFT', 'TSLA', 'GOOG'] ## Here are the quotes desired.
+	get_quotes(quotes)
